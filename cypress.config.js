96f6d3dc3eda437264d8d13a82aaa541d4dda02d
Yanina Canelo <yani_505@hotmail.com>
2025-04-09 18:18:15 -0500

    add login test

---
+++ cypress.config.js
@@ -0,0 +1,14 @@
+const { defineConfig } = require("cypress");
+
+module.exports = defineConfig({
+  e2e: {
+    setupNodeEvents(on, config) {
+      // implement node event listeners here
+      require('cypress-mochawesome-reporter/plugin')(on);
+    },
+  },
+  env: {
+    randomUsername: ''
+  },
+  reporter: 'cypress-mochawesome-reporter',
+  });
